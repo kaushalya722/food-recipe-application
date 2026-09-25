db49323733165eb09bc7e3bf5676d8487d2fa0f9
kaushalya kumari <kkaushalya722@gmail.com>
2026-09-25 13:27:22 +0530

    keep only frontend

--- backend/config/connectionDB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectionDB = async () => {
-    await mongoose.connect(process.env.DB_URL)
-    .then(() => console.log('Connected...')
-    )
-}
- module.exports=connectionDB;
