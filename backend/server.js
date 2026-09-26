ba4748852e108d3522eb34589948dc36cfb42413
kaushalya kumari <kkaushalya722@gmail.com>
2026-09-26 11:20:13 +0530

    get cors package

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config()
 const App = express();
 const connectionDB = require('./config/connectionDB')
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 App.use(express.json())
+App.use(cors())
 App.use('/recipe', require("./routes/recipe"))
 connectionDB();
 
